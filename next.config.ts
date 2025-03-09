653a4f92089c41818dcd8dae7ef5998d1fd41025
Multyp <miguel.dasilva2004@hotmail.com>
2025-03-09 14:43:13 +0100

    feat: add manifest file for Next.js app and remove static page generation timeout

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  staticPageGenerationTimeout: 300,
 };
 
 export default nextConfig;
