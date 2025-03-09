726538cf76f79e5e47e63b95f45075a7bc7db9ba
Multyp <miguel.dasilva2004@hotmail.com>
2025-03-09 14:55:56 +0100

    feat: increase static page generation timeout to 180 seconds

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  staticPageGenerationTimeout: 180,
 };
 
 export default nextConfig;
