5db6a420bbeecf7dbcc9eb587e98910b1e526941
Multyp <miguel.dasilva2004@hotmail.com>
2025-03-09 14:24:54 +0100

    chore: increase static page generation timeout for export build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  staticPageGenerationTimeout: 300,
 };
 
 export default nextConfig;
